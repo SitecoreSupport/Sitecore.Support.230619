6e05ba989f9964a376e28c6c977e2b12c8afb389
Ivan Sheyenko <ivs@sitecore.net>
2018-07-18 17:34:41 -0400

    fix

---
+++ src/Sitecore.Support.230619/ComponentServicesConfigurator.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Sitecore.DependencyInjection;
+using Sitecore.Services.Core.Security;
+using Sitecore.Services.Infrastructure.Sitecore.Security;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support.Services.Infrastructure.Sitecore.DependencyInjection
+{
+  public class ComponentServicesConfigurator : global::Sitecore.Services.Infrastructure.Sitecore.DependencyInjection.ComponentServicesConfigurator, IServicesConfigurator
+  {
+    void IServicesConfigurator.Configure(IServiceCollection serviceCollection)
+    {
+      base.Configure(serviceCollection);
+      var descriptor = serviceCollection.First(d => d.ServiceType == typeof(IUserService)); 
+      serviceCollection.Remove(descriptor);
+      serviceCollection.AddScoped<IUserService, global::Sitecore.Support.Services.Infrastructure.Sitecore.Security.UserServiceEx>();
+    }
+  }
+}
---
+++ src/Sitecore.Support.230619/UserService.cs
@@ -0,0 +1,26 @@
+﻿using Sitecore.Security.Accounts;
+using Sitecore.Services.Core.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support.Services.Infrastructure.Sitecore.Security
+{
+  public class UserServiceEx : global::Sitecore.Services.Infrastructure.Sitecore.Security.UserService, IUserService
+  {
+    bool IUserService.IsAnonymousUser
+    {
+      get
+      {
+        if (Context.User != null && Context.User.IsAuthenticated)
+        {
+          return false;
+        }
+        return true;
+      }
+    }
+
+   
+  }
+}
