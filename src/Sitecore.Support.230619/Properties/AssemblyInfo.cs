df3197f990a89704e8a08090faf35cd43cec8050
Ivan Sheyenko <ivs@sitecore.net>
2018-07-18 16:23:23 -0400

    init

---
+++ src/Sitecore.Support.230619/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.230619")]
+[assembly: AssemblyProduct("Sitecore.Support.230619")]
+[assembly: ComVisible(false)]
